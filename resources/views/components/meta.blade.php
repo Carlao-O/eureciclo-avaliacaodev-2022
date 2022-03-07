04dc4696c5d2d64de3d3e5b873082457b5b91da6
Carlos Augusto <carlosaugusto.aesouza@gmail.com>
2022-03-07 13:43:17 -0300

    :construction: Cria componente para Meta Tags

---
+++ resources/views/components/meta.blade.php
@@ -0,0 +1,17 @@
+<meta name="author" content="Eu Reciclo - Desafio de Programação">
+<meta name="description" content="Desafio de Programação da Eu Reciclo para a avaliação das habilidades de candidatos a vagas de desenvolvedor remoto." />
+<meta name="keywords" content="Eu Reciclo, Desafio de Programação, PHP, Laravel" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+<meta property="og:title" content="Eu Reciclo - Desafio de Programação">
+<meta property="og:description" content="Desafio de Programação da Eu Reciclo para a avaliação das habilidades de candidatos a vagas de desenvolvedor remoto.">
+<meta property="og:url" content="https://eureciclo.com.br">
+<meta property="og:locale" content="pt_BR" />
+<meta property="og:image" content="{{ asset('assets/images/commom/logos/logo_colors.png') }}">
+<meta property="og:type" content="website">
+
+<meta property="twitter:title" content="Eu Reciclo - Desafio de Programação">
+<meta property="twitter:description" content="Desafio de Programação da Eu Reciclo para a avaliação das habilidades de candidatos a vagas de desenvolvedor remoto.">
+<meta property="twitter:url" content="https://eureciclo.com.br">
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:image" content="{{ asset('assets/images/commom/logos/logo_colors.png') }}">
