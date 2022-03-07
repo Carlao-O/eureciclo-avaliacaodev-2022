4555d82a40897a625b015b0ec2141a9c68956528
Carlos Augusto <carlosaugusto.aesouza@gmail.com>
2022-03-07 16:09:29 -0300

    :construction: Cria Repository para Arquivos
    A classe FileRepository centraliza a lógica de acesso a dados da Model File.

---
+++ app/Repositories/FileRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\File;
+
+class FileRepository extends BaseRepository
+{
+	public function __construct(File $model)
+	{
+		$this->model = $model;
+	}
+}
--- app/Services/FileService.php
+++ app/Services/FileService.php
@@ -2,7 +2,12 @@
 
 namespace App\Services;
 
+use App\Repositories\FileRepository;
+
 class FileService
 {
-    
+    public function __construct(FileRepository $repository)
+    {
+        $this->repository = $repository;
+    }
 }
